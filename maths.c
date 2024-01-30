1bd6d0161d1c630ed040dd0c731dfbb6017f33b6
Gargi Misra <86353928+GargiMisra@users.noreply.github.com>
2024-01-30 15:20:40 +0530

    Update maths.c

--- maths.c
+++ maths.c
@@ -5,3 +5,12 @@ int add(int a, int b){
 int sub(int a, int b){
   return a-b;
 }
+int mul(int a, int b){
+  return a*b;
+}
+int div(int a,int b){
+  return a/b;
+}
+int rem(int a,int b){
+  return a%b;
+}
